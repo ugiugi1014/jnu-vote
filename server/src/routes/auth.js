ccd13569b8b024e2e4bdc4333051cde6ac242650
Song Jae Uk <ugiugi1014@gamil.com>
2026-04-14 20:36:45 +0900

    fix: Gmail SMTP TLS 인증서 검증 우회 (개발환경)

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -11,6 +11,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 // 6자리 랜덤 코드 생성
